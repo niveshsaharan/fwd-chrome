d1e306b20a6eef9283c68b6dcb3047916dd9827b
Nivesh Saharan <niveshsaharan@gmail.com>
2020-10-19 12:51:09 +0530

    Version 0.0.1

--- content_web.js
+++ content_web.js
@@ -1,325 +1,213 @@
-const isWorking = false;
-const servicesBySize = {
-    '12x15x1' : [
-        {
-            service: 'USPS Priority Mail',
-            serviceId: 13,
-            package: 'Package',
-            packageId: 3,
-        },
-        {
-            service: 'FedEx 2Day®',
-            serviceId: 52,
-            package: 'FedEx One Rate® Pak',
-            packageId: 505,
-            price: 7.65, // will change in November
-        }
-    ],
-    '14x12x3' : [
-        {
-            service: 'USPS Priority Mail',
-            serviceId: 13,
-            package: 'Package',
-            packageId: 3,
-        },
-        {
-            service: 'UPS 2nd Day Air®',
-            serviceId: 28,
-            package: 'Package',
-            packageId: 3,
-            price: 8.6, // will change in November
-        }
-    ]
-}
+(function(){
+    const serviceMappings = {
+        '12x15x1' : [
+            {
+                service: 'USPS Priority Mail',
+                serviceId: 13,
+                package: 'Package',
+                packageId: 3,
+                length: 12,
+                width: 15,
+                height: 1,
+                providerId: 2,
+                carrierId: 1,
+            },
+            {
+                service: 'FedEx 2Day®',
+                serviceId: 52,
+                package: 'FedEx One Rate® Pak',
+                packageId: 505,
+                length: 12,
+                width: 15,
+                height: 1,
+                providerId: 4,
+                carrierId: 4,
+            }
+        ],
+        '14x12x3' : [
+            {
+                service: 'USPS Priority Mail',
+                serviceId: 13,
+                package: 'Package',
+                packageId: 3,
+                length: 14,
+                width: 12,
+                height: 3,
+                providerId: 2,
+                carrierId: 1,
+            },
+            {
+                service: 'UPS 2nd Day Air®',
+                serviceId: 28,
+                package: 'Package',
+                packageId: 3,
+                length: 14,
+                width: 12,
+                height: 3,
+                providerId: 3,
+                carrierId: 3,
+            }
+        ]
+    }
 
-const extractPriceFromHtml = ($container, resolve, reject) => {
-    const shippingRateContainer = $container.find('.shipping-rate.updating-pending');
-    if(shippingRateContainer.hasClass('update-pending') || shippingRateContainer.hasClass('updating-rate'))
-    {
+
+
+    Backbone.$( document ).ajaxSuccess(function( event, xhr, options, data ) {
+
+        if(options.url.endsWith('/api/orders/updaterates'))
+        {
+            const requestData = JSON.parse(options.data);
+
+            if(data.final)
+            {
+                console.log('Original Request ' + data.orders[0].ServiceID + ' --- ' + data.orders[0].RequestedPackageTypeID, requestData, data)
+            }
+
+            getShippingRatesForServices(requestData, data);
+        }
+
+        if(window.fwdPaused)
+        {
+            return;
+        }
+
+        if(options.url.endsWith('/api/orders/BulkUpdate'))
+        {
+            $('.modal.order-detail .get-quote').click();
+        }
+
+        if(options.url.includes('/api/shipments/List?orderID='))
+        {
+            $('.modal.order-detail .get-quote').click();
+        }
+    });
+
+    function setCheapestServiceAsSelected(data, service){
+        const $container = $('.modal.order-detail');
+
+        if($container.find('[name="ServiceID"]').val() != service.serviceId)
+        {
+            $container.find('[name="ServiceID"]').val(service.serviceId).trigger('change');
+        }
+
         setTimeout(function(){
-            extractPriceFromHtml($container, resolve, reject);
+            if($container.find('[name="RequestedPackageTypeID"]').val() != service.packageId)
+            {
+                $container.find('[name="RequestedPackageTypeID"]').val(service.packageId).trigger('change');
+            }
         }, 2000);
     }
-    else{
-        let price = $container.find('.current-rate.input-group-addon').text();
 
-        if(price)
+    async function getShippingRatesForServices(data, responseData){
+
+        if(window.fwdPaused)
         {
-            price = parseFloat(price.match(/\d+(?:\.\d+)?/g));
+            return;
+        }
 
-            if(price > 0)
+        $('#cheapest-service').remove();
+
+        const serviceMappingWithPrices = JSON.parse(JSON.stringify(serviceMappings));
+
+        const length = data.orderViews[0].Length;
+        const width = data.orderViews[0].Width;
+        const height = data.orderViews[0].Height;
+
+        const size = length + 'x' + width + 'x' + height
+
+        if(typeof serviceMappingWithPrices[size] === "object") {
+            const services = serviceMappingWithPrices[size];
+
+            for(const service of services)
             {
-                resolve(price);
+                if(responseData && responseData.final && responseData.success && responseData.orders && responseData.orders.length)
+                {
+                    setServiceRateFromResponse(responseData, services)
+                }
+
+                if(typeof service.order === "undefined")
+                {
+                    data.lowPriority = false;
+                    data.orderViews[0].ServiceID = service.serviceId;
+                    data.orderViews[0].RequestedPackageTypeID = service.packageId;
+                    data.orderViews[0].ProviderID = service.providerId;
+                    data.orderViews[0].CarrierID = service.carrierId;
+                    data.orderViews[0].Rate = 0;
+                    data.orderViews[0].RateError = null;
+                    data.orderViews[0].RatingRequestPending= false;
+                    data.orderViews[0].UpdatedRate=  true;
+
+                    let res = await fetch("https://ss4.shipstation.com/api/orders/updaterates?nivesh", {
+                        "headers": {
+                            "accept": "application/json",
+                            "content-type": "application/json; charset=UTF-8",
+                        },
+                        "body": JSON.stringify(data),
+                        "method": "POST",
+                        "credentials": "include"
+                    });
+
+                    res = await res.json();
+
+                    console.log('Manual Request ' + res.orders[0].ServiceID + ' --- ' + res.orders[0].RequestedPackageTypeID, data, res)
+
+                    setServiceRateFromResponse(res, services)
+                }
             }
-            else{
-                reject(null);
-            }
+
+            handleServiceRates(services, (service) => setCheapestServiceAsSelected(data, service));
         }
     }
-}
 
-function ensureServiceIsSelected(service, threadId, callback){
+    function setServiceRateFromResponse(response, services){
 
-    if(! shouldProceed(threadId)){
-        return;
-    }
-
-    const $container = $('.modal.order-detail')
-
-    $container.find('[name="ServiceID"]').val(service.serviceId).trigger('change');
-
-    if($container.find('[name="ServiceID"]').val() != service.serviceId)
-    {
-        setTimeout(function(){
-            ensureServiceIsSelected(service, threadId, callback);
-        }, 1000)
-    }
-    else{
-        callback(service, threadId);
-    }
-}
-
-function  ensurePackageIsSelected(service, threadId, callback) {
-
-    if(! shouldProceed(threadId)){
-        return;
-    }
-
-    const $container = $('.modal.order-detail');
-
-    $container.find('[name="RequestedPackageTypeID"]').val(service.packageId).trigger('change');
-
-    if($container.find('[name="RequestedPackageTypeID"] option[value="' + service.packageId + '"]').length === 0)
-    {
-        $container.find('[name="ServiceID"]').trigger('change');
-
-        setTimeout(function(){
-            ensurePackageIsSelected(service, threadId, callback)
-        }, 1000);
-    }
-    else if( $container.find('[name="RequestedPackageTypeID"]').val() != service.packageId)
-    {
-        setTimeout(function(){
-            ensurePackageIsSelected(service, threadId, callback)
-        }, 1000);
-    }
-    else{
-        callback(service, threadId)
-    }
-}
-
-const findRateForService = (service, threadId, callback) => {
-
-    if(! shouldProceed(threadId)){
-        return;
-    }
-
-    const $container = $('.modal.order-detail');
-
-    const shippingRateContainer = $container.find('.shipping-rate');
-
-    if(shippingRateContainer.hasClass('update-pending') || shippingRateContainer.hasClass('updating-rate'))
-    {
-        setTimeout(function(){
-            findRateForService(service, threadId, callback);
-        }, 1000);
-    }
-    else{
-        let price = $container.find('.current-rate.input-group-addon').text();
-
-        if(price)
+        if(response && response.final && response.success && response.orders && response.orders.length)
         {
-            price = parseFloat(price.match(/\d+(?:\.\d+)?/g));
+            services.forEach(service => {
+              if( parseInt(service.length, 10) === parseInt(response.orders[0].Length, 10)
+                  && parseInt(service.width, 10) === parseInt(response.orders[0].Width, 10)
+                  && parseInt(service.height, 10) === parseInt(response.orders[0].Height, 10)
+                  && parseInt(response.orders[0].ServiceID, 10) === parseInt(service.serviceId, 10)
+                  && parseInt(response.orders[0].RequestedPackageTypeID, 10) === parseInt(service.packageId, 10))
+              {
+                  service.order = response.orders[0];
 
-            if(price > 0)
-            {
-                callback(price, threadId);
-            }
-            else{
-                callback(null, threadId);
-            }
-        }
-    }
-};
-
-const cheapestService = ( prices, threadId ) => {
-
-    if(! shouldProceed(threadId)){
-        return;
-    }
-
-    const $container = $('.modal.order-detail');
-    logger('Services with prices', prices)
-
-    const service = prices.reduce((prev, curr) => prev.price < curr.price ? prev : curr);
-
-    if(service && service.price)
-    {
-        $container.find('.shipping-rate').after(`
-                    <div id="cheapest-price-message" style="font-size: 12px; color: green;"><strong>${service.service}</strong> with <strong>${service.package}</strong> is cheapest for <strong>$${service.price}</strong>.</div>
-                `)
-
-        ensureServiceIsSelected(service, threadId, function(service, threadId) {
-            ensurePackageIsSelected(service, threadId, function (service, threadId) {
-                findRateForService(service, threadId, function (price, threadId) {
-
-                    if (price) {
-
-                    }
-                })
+                  if(! response.orders[0].RateError)
+                  {
+                      service.price = response.orders[0].ShippingCost + response.orders[0].ConfirmationCost + response.orders[0].InsuranceCost + response.orders[0].OtherCost;
+                  }
+              }
             })
-        })
-    }
-    else
-    {
-        alert("Something went wrong!")
-    }
-}
+        }
 
-
-let prices = []
-let startedAt = null;
-let currentlyWorking = 0;
-
-function logger(){
-    console.log('Nivesh : ', ...arguments)
-}
-
-function shouldProceed(threadId){
-    if((threadId && currentlyWorking && threadId < currentlyWorking))
-    {
-        logger('Stop process for thread ' + threadId + ' ' + currentlyWorking )
+        return services;
     }
 
-    return !(threadId && currentlyWorking && threadId < currentlyWorking);
-}
+    function handleServiceRates(services, callback){
 
-function start(serviceIndex = 0, threadId){
-    const $container = $('.modal.order-detail')
+        const service = services.filter(service => service.price > 0).reduce((prev, curr) => prev.price < curr.price ? prev : curr);
 
-    if(! $container || $container.length === 0)
-    {
-        alert("FWD SS Tool - It can only work on the orders modal.")
-        return;
-    }
-
-    const length = $container.find('[name="LengthIn"]').val()
-    const width = $container.find('[name="WidthIn"]').val()
-    const height = $container.find('[name="HeightIn"]').val()
-
-    if(! length || ! width || ! height){
-        alert("FWD SS Tool - Please fill in all the dimensions");
-        return;
-    }
-
-    $container.find('#cheapest-price-message').remove();
-
-    const size = length + 'x' + width + 'x' + height
-
-    if(! shouldProceed(threadId)){
-        return;
-    }
-
-    // Reset prices if starting
-    if(serviceIndex === 0)
-    {
-        prices = [];
-        currentlyWorking = new Date().getTime();
-        threadId = currentlyWorking;
-        $container.find('#cheapest-price-message').remove();
-    }
-
-    if(typeof servicesBySize[size] === "object"){
-        const services = servicesBySize[size];
-
-        const nextOptionIndex = typeof services[serviceIndex + 1] === "object" ? serviceIndex + 1 : null;
-        const service = services[serviceIndex];
-
-        if(service.price > 0)
+        if(service && service.price > 0)
         {
-            logger('Fixed price service  ', service)
+            const $container = $('.modal.order-detail');
 
-            prices.push({
-                ...services[serviceIndex],
-            });
+            $('#cheapest-service').remove();
 
-            if (nextOptionIndex) {
-                start(nextOptionIndex, threadId);
-            } else {
-                cheapestService(prices, threadId)
-            }
+            $container.find('.shipping-rate').after(`
+<fieldset class="rating" id="cheapest-service" sty>
+    <div class="form-group">
+        <label class="control-label col-sm-3">Cheapest</label>
+        <div class="col-sm-9" style="color: #6ba03a; padding: 7px 9px;">
+            <span>$${service.price} (${service.service}, ${service.package}, ${service.length}x${service.width}x${service.height})</span>
+        </div>
+    </div>
+</fieldset>
+    `)
+
+            callback(service)
         }
         else{
-            logger('Find rates for service  ', service)
-
-            // Select service
-            ensureServiceIsSelected(service, threadId, function(service){
-                ensurePackageIsSelected(service, threadId, function(service){
-                    findRateForService(service, threadId, function(price) {
-
-                        if(! shouldProceed(threadId)){
-                            return;
-                        }
-
-                        if (price) {
-                            prices.push({
-                                ...services[serviceIndex],
-                                price,
-                            });
-
-                            if (nextOptionIndex) {
-                                start(nextOptionIndex, threadId);
-                            } else {
-                                cheapestService(prices, threadId)
-                            }
-                        } else {
-                            if (nextOptionIndex) {
-                                start(nextOptionIndex, threadId);
-                            } else {
-                                cheapestService(prices, threadId)
-                            }
-                        }
-                    })
-                });
-            });
-        }
-
-    }
-    else{
-        alert("FWD SS Tool - No group defined for these dimensions.")
-    }
-}
-
-function waitForTheCurrentFetchAndThen(callback){
-    const $container = $('.modal.order-detail');
-
-    const shippingRateContainer = $container.find('.shipping-rate');
-
-    if(shippingRateContainer.hasClass('update-pending') || shippingRateContainer.hasClass('updating-rate'))
-    {
-        setTimeout(function(){
-            waitForTheCurrentFetchAndThen(callback);
-        }, 1000);
-    }
-    else{
-        callback();
-    }
-}
-
-$(document).on('keyup', function(e){
-    if(e.ctrlKey || e.metaKey)
-    {
-        if([56, 57].includes(e.which))
-        {
-            setTimeout(function() {
-                waitForTheCurrentFetchAndThen(function () {
-                    start()
-                })
-            }, 3000)
+            console.log('Something went wrong.', services, service)
         }
     }
-});
-
-$(document).on('dblclick', '.modal.order-detail .current-rate', function(){
-    start();
-});
+})()
